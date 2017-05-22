5d32337866b0bd3ed233ebb01375c40722c6dcc2
kidsodsadza <khantong_n@silpakorn.edu>
2017-05-23 01:13:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>google</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="boxfacebook">
+<div class="mid">
+<?php
+//=========== facebook ===========
+include_once('google-config.php');
+include_once('google-login.php');
+
+?>
+</div>
+</div>
+
+
+</body>
+</html>
